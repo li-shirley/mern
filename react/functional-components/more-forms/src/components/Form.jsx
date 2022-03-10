c7e8e3ce665578fe69bb27eb95d56fb93a8b2313
Shirley Li <lishirley@me.com>
2022-03-10 14:59:47 -0800

    changed if statements to ternerary

--- react/functional-components/more-forms/src/components/Form.jsx
+++ react/functional-components/more-forms/src/components/Form.jsx
@@ -14,49 +14,44 @@ const Form = (props) => {
 
     const handleFirstName = (e) => {
         setFirstName(e.target.value);
-        if (e.target.value.length < 1) {
+        // if (e.target.value.length < 2 && e.target.value.length > 0) {
+        //     setFirstNameError("First Name must be 2 characters or longer!");
+        // } else {
+        //     setFirstNameError("");
+        // } 
+
+        (e.target.value.length < 2 && e.target.value.length > 0) ?
+            setFirstNameError("First Name must be 2 characters or longer!") :
             setFirstNameError("");
-        } else if (e.target.value.length < 2) {
-            setFirstNameError("First Name must be 2 characters or longer!");
-        } else {
-            setFirstNameError("");
-        } 
+    
     }
 
     const handleLastName = (e) => {
         setLastName(e.target.value);
-        if (e.target.value.length < 1) {
+        (e.target.value.length < 2 && e.target.value.length > 0) ?
+            setLastNameError("Last Name must be 2 characters or longer!") :
             setLastNameError("");
-        } else if (e.target.value.length < 2) {
-            setLastNameError("Last Name must be 2 characters or longer!");
-        } else setLastNameError("");
     }
 
     const handleEmail = (e) => {
         setEmail(e.target.value);
-        if (e.target.value.length < 1) {
+        (e.target.value.length < 5 && e.target.value.length > 0) ?
+            setEmailError("Email must be 5 characters or longer!") :
             setEmailError("");
-        } else if (e.target.value.length < 5) {
-            setEmailError("Email must be 5 characters or longer!");
-        } else setEmailError("");
     }
 
     const handlePassword = (e) => {
         setPassword(e.target.value);
-        if (e.target.value.length < 1) {
+        (e.target.value.length < 8 && e.target.value.length > 0) ?
+            setPasswordError("Password must be 8 characters or longer!") :
             setPasswordError("");
-        } else if (e.target.value.length < 8) {
-            setPasswordError("Password must be 8 characters or longer!");
-        } else setPasswordError("");
     }
 
     const handleConfirmPassword = (e) => {
         setConfirmPassword(e.target.value);
-        if (e.target.value.length < 1) {
+        (e.target.value !== password && e.target.value.length > 0) ?
+            setConfirmPasswordError("Passwords must match!") :
             setConfirmPasswordError("");
-        } else if (e.target.value !== password) {
-            setConfirmPasswordError("Passwords must match!");
-        } else setConfirmPasswordError("");
     }
 
     return (
