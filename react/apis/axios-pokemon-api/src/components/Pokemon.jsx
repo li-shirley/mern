394c86de9a23cf1baebdfc70c1f4b4448350056b
Shirley Li <lishirley@me.com>
2022-03-15 12:34:13 -0700

    "Heidi bonus" + styling

--- react/apis/axios-pokemon-api/src/components/Pokemon.jsx
+++ react/apis/axios-pokemon-api/src/components/Pokemon.jsx
@@ -8,13 +8,15 @@ const Pokemon = () => {
 
     const handleFetchAll = () => {
         axios.get('https://pokeapi.co/api/v2/pokemon?limit=807')
-            .then(response => { setPokemons(response.data.results) })
+            .then(response => { 
+                console.log(response)
+                setPokemons(response.data.results) })
             .catch(error => console.log(error))
     };
 
     const handleFetchOne = (e) => {
         e.preventDefault();
-        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName.toLowerCase()}`)
             .then(response => { 
                 console.log(response.data)
                 setPokemon(response.data) })
@@ -24,34 +26,35 @@ const Pokemon = () => {
 
     return (
         <div>
-            <div>
+            <div className="container w-50 shadow p-3 mb-5 bg-body rounded">
                 <form onSubmit={handleFetchOne}>
-                    <label>
-                        Fetch a Pokemon of your choice:
-                    </label>
-                    <input type="text" name="pokemonName" placeholder="Enter Pokemon name" onChange={(e) => setPokemonName(e.target.value)} value={pokemonName}>
+                    <h3>Fetch a Pokemon of your choice</h3>
+                    <input className="form-control" type="text" name="pokemonName" placeholder="Enter Pokemon name" onChange={(e) => setPokemonName(e.target.value)} value={pokemonName}>
                     </input>
-                    <button>Submit</button>
+                    <button className="btn btn-success my-2">Submit</button>
                 </form>
                 <div>
                     {
                         pokemon &&
                         <div>
-                            <p>{pokemon.name}</p>
+                            <p>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</p>
                             <img src={pokemon.sprites.front_default}></img>
                         </div>
                     }
                 </div>
             </div>
-            <div>
-                <button onClick={handleFetchAll} className="btn btn-secondary">See All the Pokemon!</button>
+            <div className="container w-50 shadow p-3 mb-5 bg-body rounded">
+                <h3>Get a list of all the Pokemon</h3>
+                <button onClick={handleFetchAll} className="btn btn-secondary">Gotta catch 'em all!</button>
                 {
                     pokemons &&
                     pokemons.map((pokemon, i) => {
                         return (
                             <div key={i}>
                                 <ul>
-                                    <li>{pokemon.name}</li>
+                                    <li>
+                                        <span>{pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)} </span>
+                                    </li>
                                 </ul>
                             </div>)
                     })}
